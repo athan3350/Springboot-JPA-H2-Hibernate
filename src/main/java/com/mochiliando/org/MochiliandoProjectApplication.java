732171d87f1826c167537fc3d63eb83dda3045e3
Sebastian Suarez <ssuarez@yellowpepper.com>
2022-09-01 15:55:10 -0500

    :tada: Initial commit

---
+++ src/main/java/com/mochiliando/org/MochiliandoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mochiliando.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MochiliandoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MochiliandoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mochiliando/org/MochiliandoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mochiliando.org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MochiliandoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
