cc75ddcaeee80dd71625a25389536aca8f5d1a3b
Sebastian Suarez <ssuarez@yellowpepper.com>
2022-09-14 15:11:15 -0500

    :memo: Implement Logic to support swagger 2 documentation

--- src/main/java/com/mochiliando/org/MochiliandoProjectApplication.java
+++ src/main/java/com/mochiliando/org/MochiliandoProjectApplication.java
@@ -2,8 +2,10 @@ package com.mochiliando.org;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class MochiliandoProjectApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/mochiliando/org/controler/PropertyController.java
+++ src/main/java/com/mochiliando/org/controler/PropertyController.java
@@ -2,6 +2,8 @@ package com.mochiliando.org.controler;
 
 import com.mochiliando.org.model.dto.PropertyDTO;
 import com.mochiliando.org.service.PropertyService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,34 +11,37 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = "Property APIs")
 @RestController
 @RequestMapping("/api/v1/properties")
 public class PropertyController {
 
     @Autowired
     PropertyService propertyService;
-
-    @PostMapping("")
+    @ApiOperation(value = "Save property", notes = "This method is used to save a property")
+    @PostMapping(path = "", consumes = {"application/json"}, produces = {"application/json"})
     public ResponseEntity<PropertyDTO> saveProperty (@RequestBody PropertyDTO propertyDTO) {
         propertyDTO = propertyService.saveProperty(propertyDTO);
         return new ResponseEntity<>(propertyDTO, HttpStatus.CREATED);
     }
 
-    @GetMapping("")
+    @ApiOperation(value = "Get all properties", notes = "This method is used to get all properties")
+    @GetMapping(path = "", consumes = {"application/json"}, produces = {"application/json"})
     public ResponseEntity<List<PropertyDTO>> getAllProperties () {
         return new ResponseEntity<>(propertyService.getAllProperties(), HttpStatus.OK);
     }
 
-    @PutMapping("/{propertyId}")
+    @ApiOperation(value = "Update property", notes = "This method is used to update an specific property")
+    @PutMapping(path = "/{propertyId}", consumes = {"application/json"}, produces = {"application/json"})
     public ResponseEntity<PropertyDTO> updateProperty(@RequestBody PropertyDTO propertyDTO, @PathVariable Long propertyId ) {
         return new ResponseEntity<>(propertyService.updateProperty(propertyDTO, propertyId), HttpStatus.CREATED);
     }
 
-    @DeleteMapping("/{propertyId}")
+    @ApiOperation(value = "Delete property", notes = "This method is used to delete an specific property")
+    @DeleteMapping(path = "/{propertyId}", consumes = {"application/json"}, produces = {"application/json"})
     public ResponseEntity<Void> deletePropertyById ( @PathVariable Long propertyId ) {
         propertyService.deletePropertyById(propertyId);
         return ResponseEntity.noContent().build();
     }
-
 
 }
